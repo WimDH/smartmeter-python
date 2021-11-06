863c4d2067c13f9dff0c748bd1bc347bc47cb1f4
Wim De Hul <wim.de.hul+gitlab@gmail.com>
2021-11-06 13:48:56 +0100

    fix serial port

--- app/digimeter.py
+++ app/digimeter.py
@@ -106,41 +106,34 @@ def read_serial(
     ) as serial_port:
         try:
             LOG.debug(f"Reading from serial port '{port}'.")
-            while True:
-                # Read data from port
-                line = serial_port.readline()
+            while serial_port.inWaiting() > 0:
+                try:
+                    # Read data from port
+                    line = serial_port.readline()
 
-                if start_of_telegram.search(line.decode("ascii")):  # Start of message
-                    LOG.debug("Start of message deteced.")
-                    telegram = bytearray()
-                    start_of_telegram_detected = True
+                    if start_of_telegram.search(line.decode("ascii")):  # Start of message
+                        LOG.debug("Start of message deteced.")
+                        telegram = bytearray()
+                        start_of_telegram_detected = True
 
-                if start_of_telegram_detected:
-                    telegram += line
+                    if start_of_telegram_detected:
+                        telegram += line
 
-                if end_of_telegram.search(line.decode("ascii")):  # End of message
-                    LOG.debug("End of message deteced")
-                    telegram_count += 1
-                    start_of_telegram_detected = False
-                    LOG.debug("Recorded a new telegram:{}". format(telegram.decode("ascii")))
+                    if end_of_telegram.search(line.decode("ascii")):  # End of message
+                        LOG.debug("End of message deteced")
+                        telegram_count += 1
+                        start_of_telegram_detected = False
+                        LOG.debug("Recorded a new telegram:{}". format(telegram.decode("ascii")))
 
-                    if check_msg(telegram):
-                        # If the CRC is correct, add it to the queue.
-                        LOG.debug("Parsing telegram.")
-                        queue_data = parse(telegram.decode())
-                        LOG.debug("Add parsed data to the queue.")
-                        msg_q.put(queue_data)
+                        if check_msg(telegram):
+                            # If the CRC is correct, add it to the queue.
+                            LOG.debug("Parsing telegram.")
+                            queue_data = parse(telegram.decode())
+                            LOG.debug("Add parsed data to the queue.")
+                            msg_q.put(queue_data)
 
-                if _quit_after and _quit_after == telegram_count:
-                    break
-
-        except SerialException as se:
-            LOG.critical(
-                f"Unable to configure serial port '{port}' with settings '{baudrate},{bytesize},{parity},{stopbits}'."
-            )
-            LOG.exception(se)
-
-        finally:
-            if serial_port:
-                serial_port.flush()
-                serial_port.close()
+                    if _quit_after and _quit_after == telegram_count:
+                        break
+                
+                except SerialException:
+                    pass
